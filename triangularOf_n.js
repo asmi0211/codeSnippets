b01c39bdbba8ad04c3c6270ec37cd8d16749f0b3
asmi0211 <52039722+asmi0211@users.noreply.github.com>
2024-11-21 18:37:56 +0530

    Create triangularOf_n.js

---
+++ triangularOf_n.js
@@ -0,0 +1,10 @@
+// Return the nth triangular number
+function triangular( n ) { // function definition
+    if(n<=0){ 
+        return 0 
+    }
+    else{  
+   var result = ( n * (n + 1) / 2); 
+    }
+   return result 
+}
